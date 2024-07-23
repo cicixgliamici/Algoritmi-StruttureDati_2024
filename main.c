28c0fe9c491fee9516a8ae31e4abeb1adb12976e
Leonardo <10720295@polimi.it>
2024-07-23 17:51:20 +0200

    Strutture Dati?

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ typedef struct {
 
 void heapify(MinHeapIngrediente* heap, int i);
 void inserisciIngredienteHeap(MinHeapIngrediente* heap, int scadenza, int quantita);
-void liberaMinHeap(MinHeapIngrediente* heap);
+void liberaMinHeapIngredienti(MinHeapIngrediente* heap);
 IngredienteMinHeap rimuoviIngredienteHeap(MinHeapIngrediente* heap);
 MinHeapIngrediente creaHeapIngredienti(int capacita);
 
@@ -35,7 +35,107 @@ typedef struct NodoAVL {
     int altezza;
 } NodoAVL;
 
+void inOrder(NodoAVL *root);
+void liberaAVL(NodoAVL *root);
+int getBilancio(NodoAVL *nodo);
+int altezza(NodoAVL *nodo);
+int max(int a, int b);
+NodoAVL* nuovoNodo(char* nome, int capacita);
+NodoAVL* ruotaDestra(NodoAVL *y);
+NodoAVL* ruotaSinistra(NodoAVL *x);
+NodoAVL* minValueNode(NodoAVL *nodo);
+NodoAVL* elimina(NodoAVL* root, char *nome);
+NodoAVL* inserisci(NodoAVL* nodo, char* nome, int scadenza, int quantita, int capacita);
 
+//Ricette - Lista
+typedef struct IngredienteRicetta {
+    char nome[256];
+    int quantita;
+    struct IngredienteRicetta *next;
+} IngredienteRicetta;
+
+typedef struct Ricetta {
+    char nome[256];
+    IngredienteRicetta *ingredienti;
+} Ricetta;
+
+//Ricette - BST
+typedef struct NodoBST {
+    Ricetta ricetta;
+    struct NodoBST *sinistro;
+    struct NodoBST *destro;
+} NodoBST;
+
+void liberaBST(NodoBST* root);
+void inOrderRicetta(NodoBST* root);
+void liberaListaIngredienti(IngredienteRicetta* ingrediente);
+NodoBST* nuovoNodoRicetta(Ricetta ricetta);
+NodoBST* minValueNodoRicetta(NodoBST* nodo);
+NodoBST* inserisciRicetta(NodoBST* nodo, Ricetta ricetta);
+NodoBST* cercaRicetta(NodoBST* nodo, char* nome);
+NodoBST* eliminaRicetta(NodoBST* root, char* nome);
+
+//Ordine da Fare - Coda FIFO
+typedef struct Ordine {
+    char nome_ricetta[256];
+    int quantita;
+    int tempo_arrivo;
+    struct Ordine* next;
+} Ordine;
+
+typedef struct {
+    Ordine* testa;
+    Ordine* coda;
+} CodaOrdini;
+
+void liberaCoda(CodaOrdini* coda);
+void aggiungiOrdine(CodaOrdini* coda, char* nome_ricetta, int quantita, int tempo_arrivo);
+int codaVuota(CodaOrdini* coda);
+CodaOrdini* creaCoda();
+Ordine* rimuoviOrdine(CodaOrdini* coda);
+
+//Ordini fatti - minHeap
+typedef struct OrdineHeap {
+    int tempo_arrivo;
+    char nome_ricetta[256];
+    int quantita;
+} OrdineHeap;
+
+typedef struct {
+    OrdineHeap* ordini;
+    int dimensione;
+    int capacita;
+} MinHeap;
+
+void liberaMinHeapOrdini(MinHeap* heap);
+void scambiaOrdini(OrdineHeap* a, OrdineHeap* b);
+void heapifyOrdini(MinHeap* heap, int i);
+void inserisciOrdineHeap(MinHeap* heap, int tempo_arrivo, char* nome_ricetta, int quantita);
+int heapVuoto(MinHeap* heap);
+MinHeap creaMinHeap(int capacita);
+OrdineHeap* rimuoviMin(MinHeap* heap);
+
+//Spedizione - maxHeap
+typedef struct Spedizione {
+    char nome[256];
+    int istante_arrivo;
+    int quantita;
+    int peso;
+} Spedizione;
+
+typedef struct {
+    Spedizione* spedizioni;
+    int dimensione;
+    int capacità;
+} MaxHeapSpedizioni;
+
+void heapifySpedizioni(MaxHeapSpedizioni* heap, int i);
+void inserisciSpedizione(MaxHeapSpedizioni* heap, char* nome, int istante_arrivo, int quantita, int peso);
+void stampaSpedizioni(MaxHeapSpedizioni* heap);
+void liberaMaxHeap(MaxHeapSpedizioni* heap);
+int heapVuotoSpedizioni(MaxHeapSpedizioni* heap);
+MaxHeapSpedizioni creaMaxHeap(int capacita);
+Spedizione rimuoviMac(MaxHeapSpedizioni* heap);
 
 int main(void) {
     return 0;
