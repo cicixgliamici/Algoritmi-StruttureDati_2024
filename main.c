c44b56d08ef9bb5babde534a532d09a9f6a4e44c
Leonardo <10720295@polimi.it>
2024-07-23 14:50:30 +0200

    Ciaociao

--- main.c
+++ main.c
@@ -1,4 +1,42 @@
+/* 5 Strutture dati:  Ingredienti - AVL con min-Heap
+*                     Ricette - BST con liste
+*                     Ordini da Fare - Coda FIFO
+*                     Ordini fatti - min-Heap
+*                     Camioncino - max-Heap
+*/
+
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Ingredienti - Heap
+typedef struct IngredienteMinHeap {
+    int scadenza;
+    int quantita;
+}  IngredienteMinHeap;
+
+typedef struct {
+    IngredienteMinHeap* lotto;
+    int dimensione;
+    int capacita;
+} MinHeapIngrediente;
+
+void heapify(MinHeapIngrediente* heap, int i);
+void inserisciIngredienteHeap(MinHeapIngrediente* heap, int scadenza, int quantita);
+void liberaMinHeap(MinHeapIngrediente* heap);
+IngredienteMinHeap rimuoviIngredienteHeap(MinHeapIngrediente* heap);
+MinHeapIngrediente creaHeapIngredienti(int capacita);
+
+//Ingredienti - AVL
+typedef struct NodoAVL {
+    char nome[256];
+    MinHeapIngrediente heap;
+    struct NodoAVL *sinistro;
+    struct NodoAVL *destro;
+    int altezza;
+} NodoAVL;
+
+
 
 int main(void) {
     return 0;
