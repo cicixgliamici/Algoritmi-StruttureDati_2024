75eaab3f90f9c88f1f9d548645091060867ac03f
Leonardo <160722347+cicixgliamici@users.noreply.github.com>
2024-07-31 09:44:23 +0200

    Update main.c

--- main.c
+++ main.c
@@ -31,12 +31,13 @@ int max(int a, int b) {
     return (a > b) ? a : b;
 }
 
-char* leggiRiga(FILE* file) {
+//Chiamata da gestioneComandi
+char* letturaRiga(FILE* file) {
     size_t capacita = 256;
-    char *riga = (char *)malloc(capacita);
-    if (fgets(riga, capacita, file) != NULL) {
+    char *riga = (char *)malloc(capacita);                                             //Alloca memoria per la riga
+    if (fgets(riga, capacita, file) != NULL) {                                         //Allocazione a buon fine
         size_t lunghezza = strlen(riga);
-        while (lunghezza > 0 && riga[lunghezza - 1] != '\n') {
+        while (lunghezza > 0 && riga[lunghezza - 1] != '\n') {                         //Abbiamo allocato troppo poco spazio  
             capacita *= 2;
             riga = (char *)realloc(riga, capacita);
             if (fgets(riga + lunghezza, capacita - lunghezza, file) == NULL) {
@@ -44,37 +45,37 @@ char* leggiRiga(FILE* file) {
             }
             lunghezza = strlen(riga);
         }
-        riga[strcspn(riga, "\n")] = 0;  // Rimuove il carattere di nuova linea
+        riga[strcspn(riga, "\n")] = 0;                                                 //Rimuove il carattere \n
     } else {
         free(riga);
         riga = NULL;
     }
-    return riga;
+    return riga;                                                                       //Dalla al gestore
 }
 
 // Funzioni Algoritmo
-void gestisciComandi(FILE *file) {
+void gestioneComandi(FILE *file) {
     int tempoCamion, capacitaMaxHeap;
-    if(fscanf(file, "%d %d", &tempoCamion, &capienzaCamion)==2);
-    if(capienzaCamion>=100000)
+    if(fscanf(file, "%d %d", &tempoCamion, &capienzaCamion)==2);                                       //Dovuto al compilatore, vuole che venga usato il valore di fscanf 
+    if(capienzaCamion>=100000)                                                                         //Problemi di allocazione con il malloc del maxHeap
         capacitaMaxHeap = capienzaCamion / 1000;
     else
-        capacitaMaxHeap = capienzaCamion;  // Nel caso peggiore ho capienzaCamion ordini di peso 1
+        capacitaMaxHeap = capienzaCamion;                                                              //Nel caso peggiore ho capienzaCamion ordini di peso 1
     max_heap_spedizioni = creaMaxHeap(capacitaMaxHeap);
     coda_ordini = creaCoda();
     heap_ordini_fatti = creaMinHeap(10);
     char *line;
     while ((line = leggiRiga(file)) != NULL) {
         char command[256];
-        if (sscanf(line, "%s", command) == 1) {
-            tempoCorrente++;
-            if(tempoCorrente % tempoCamion == 0 && tempoCorrente != 0){
+        if (sscanf(line, "%s", command) == 1) { 
+            tempoCorrente++;                                                                          //Ad ogni nuova lettura (tempoCamion e capacita esclusi) aggiorno il tempo
+            if(tempoCorrente % tempoCamion == 0 && tempoCorrente != 0){                               //Se è un multiplo di tempoCamion(divisibile con resto zero)(zero/x però da resto zero quindi lo escludo) carico il camion
                 caricaCamion();
             }
             if (strcmp(command, "aggiungi_ricetta") == 0) {
                 char nome_ricetta[256];
-                if (sscanf(line + strlen(command), "%s", nome_ricetta) == 1) {
-                    if (cercaBST(bst, nome_ricetta) != NULL) {
+                if (sscanf(line + strlen(command), "%s", nome_ricetta) == 1) {                        //Se già c'è mi evito di leggere tutta la ricetta
+                    if (cercaBST(bst, nome_ricetta) != NULL) {                                       
                         printf("ignorato\n");
                     } else {
                         Ricetta nuova_ricetta;
@@ -84,7 +85,7 @@ void gestisciComandi(FILE *file) {
                         char ingrediente[256];
                         int quantita;
                         char *ptr = line + strlen(command) + strlen(nome_ricetta) + 1;
-                        while (sscanf(ptr, "%s %d", ingrediente, &quantita) == 2) {
+                        while (sscanf(ptr, "%s %d", ingrediente, &quantita) == 2) {                   //Finché continuo a leggere coppie string int
                             IngredienteRicetta *nuovo_ingrediente = (IngredienteRicetta*)malloc(sizeof(IngredienteRicetta));
                             strcpy(nuovo_ingrediente->nome, ingrediente);
                             nuovo_ingrediente->quantita = quantita;
@@ -106,20 +107,19 @@ void gestisciComandi(FILE *file) {
                     rimuovi_ricetta(nome_ricetta);
                 }
             } else if (strcmp(command, "rifornimento") == 0) {
-                rifornimento(line + strlen(command) + 1);
+                rifornimento(line + strlen(command) + 1);                                             //Nessun controllo, non esistono rifornimenti sbagliati
             } else if (strcmp(command, "ordine") == 0) {
                 char nome_ricetta[256];
                 int quantita;
                 if (sscanf(line + strlen(command), "%s %d", nome_ricetta, &quantita) == 2) {
-                    ordine(nome_ricetta, quantita);
+                    ordine(nome_ricetta, quantita);                                                   //Meccanica di ordine spiegata meglio dopo
                 }
             }
         }
         free(line);
     }
-    if((tempoCorrente + 1) % tempoCamion == 0 && tempoCorrente != 0)
-        caricaCamion();
-    printf("\b \b");
+    if((tempoCorrente + 1) % tempoCamion == 0 && tempoCorrente != 0)                                  //Le righe finiscono a n*(tempoCamion-1) ma io devo "leggere" comunque la prossima perché
+        caricaCamion();                                                                               //il caricaCamion va fatto prima delle 4 azioni canoniche
 }
 
 void aggiungi_ricetta(Ricetta nuova_ricetta) {
@@ -127,22 +127,22 @@ void aggiungi_ricetta(Ricetta nuova_ricetta) {
     printf("aggiunta\n");
 }
 
-void rimuovi_ricetta(const char* nome_ricetta) {
+void rimuovi_ricetta(const char* nome_ricetta) {                                          //Posso provare a cambiare ordine ai cicli per vedere se guadagno tempo
     Ordine* ordine_corrente = coda_ordini->testa;
-    while (ordine_corrente != NULL) {                                                   //Controllo nella coda degli ordini da fare
+    for (int i = 0; i < heap_ordini_fatti->dimensione; i++) {                             //Controllo nel minHeap degli ordini fatti
+        if (strcmp(heap_ordini_fatti->ordini[i].ricetta, nome_ricetta) == 0) {
+            printf("ordini in sospeso\n");
+            return;
+        }
+    }
+    while (ordine_corrente != NULL) {                                                     //Controllo nella coda degli ordini da fare
         if (strcmp(ordine_corrente->nome_ricetta, nome_ricetta) == 0) {
             printf("ordini in sospeso\n");
             return;
         }
         ordine_corrente = ordine_corrente->next;
     }
-    for (int i = 0; i < heap_ordini_fatti->dimensione; i++) {                           //Controllo nel minHeap degli ordini fatti
-        if (strcmp(heap_ordini_fatti->ordini[i].ricetta, nome_ricetta) == 0) {
-            printf("ordini in sospeso\n");
-            return;
-        }
-    }
-    if (cercaBST(bst, (char*)nome_ricetta) == NULL) {                                          //Controllo nel BST
+    if (cercaBST(bst, (char*)nome_ricetta) == NULL) {                                     //Controllo nel BST
         printf("non presente\n");
     } else {
         bst = eliminaBST(bst, (char*)nome_ricetta);
@@ -164,7 +164,7 @@ void rifornimento(const char* comando) {
         ptr += strlen(nome_ingrediente) + 1 + snprintf(NULL, 0, "%d", quantita) + 1 + snprintf(NULL, 0, "%d", scadenza) + 1;
     }
     printf("rifornito\n");
-    verificaOrdini();
+    verificaOrdini();                                                               //Ad ogni rifornimento verifico se posso liberare la coda di ordini da fare
 }
 
 // Main - Gestione del giorno
